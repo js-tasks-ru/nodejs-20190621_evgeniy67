ddcfa9aa0681bc4c0772f765c4b2148c01acf939
Gluhov Evgeny <glukhov8@yandex.ru>
2019-06-26 20:23:55 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,36 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
+function _getFile(list, value) {
+  return list.find((name) => path.parse(name).name === value);
+}
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  const dirPath = path.join(__dirname, 'files');
 
   switch (req.method) {
     case 'GET':
+      if (!path.parse(pathname).dir) {
+        const filesNames = fs.readdirSync(dirPath);
+        const file = _getFile(filesNames, pathname);
 
+        if (file) {
+          const filepath = `${dirPath}/${file}`;
+          const stream = fs.createReadStream(filepath);
+          stream.pipe(res);
+        } else {
+          res.statusCode = 404;
+          res.end('Not found');
+        }
+      } else {
+        res.statusCode = 400;
+        res.end('it is dirname');
+      }
       break;
 
     default:
