8b6522c0a0142b7e6f0994cc066581df2fb429b1
Gluhov Evgeny <glukhov8@yandex.ru>
2019-07-06 15:34:45 +0300

    add resolution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,5 +1,8 @@
 const Koa = require('koa');
+const Chat = require('./Chat');
+
 const app = new Koa();
+const chat = new Chat();
 
 app.use(require('koa-static')('public'));
 app.use(require('koa-bodyparser')());
@@ -8,9 +11,31 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  let resolveWaiting = null;
+  const waiting = new Promise((resolve) => {
+    resolveWaiting = resolve;
+  });
+
+  chat.once('change', (message) => {
+    resolveWaiting(message);
+  });
+
+  const message = await waiting;
+
+  ctx.status = 200;
+  ctx.body = message;
+
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    chat.add(ctx.request.body.message);
+    ctx.status = 200;
+    ctx.body = 'Message added';
+  }
+
+  return next();
 });
 
 app.use(router.routes());
