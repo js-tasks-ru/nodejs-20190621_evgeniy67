d2ca9c1af5fc423c5a21871add76dce7415e80cf
Gluhov Evgeny <glukhov8@yandex.ru>
2019-07-21 17:10:26 +0300

    add resolution for 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,19 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(false, false, 'Не указан email');
+
+  const user = await User.findOne({email});
+
+  if (user) {
+    return done(null, user);
+  }
+
+  const u = await new User({email, displayName});
+  await u.save().catch(async (err) => {
+    await u.delete();
+    done(err, false, 'Некорректный email.');
+  });
+
+  return done(null, u);
 };
